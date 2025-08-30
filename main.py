90850a0b407a3a5ad3e6ad9f8e957d62d87888a3
murattestapp <murat.ticaret@yahoo.com.tr>
2025-08-30 20:31:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 from typing import List, Optional
